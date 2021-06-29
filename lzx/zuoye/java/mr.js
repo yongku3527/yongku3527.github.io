f3d119e7736fb5b710d497424ad5330f63a997bb
yongku3527 <86635269+jacjjack@users.noreply.github.com>
2021-06-29 17:10:10 +0800

    11

---
+++ lzx/zuoye/java/mr.js
@@ -0,0 +1,14 @@
+var video;
+function int(){
+    video=document.getElementById("video1");
+    video.addEventListener("ended",function() {alert("播放结束。");},true);
+}
+function play(){
+    video.play();
+}
+function pause(){
+    video.pause();
+}
+function load(){
+    video.load();
+}
